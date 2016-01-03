1d8c26f0a81da2dfd3613079f35e756ee8ce8474
xxu52 <xxu52@mir.cs.illinois.edu>
2016-01-03 15:49:14 -0600

    Commit legacy code

---
+++ projects/tox4j/src/main/java/im/tox/hlapi/adapter/parser/Parser.scala
@@ -0,0 +1,15 @@
+package im.tox.hlapi.adapter.parser
+
+import im.tox.hlapi.state.CoreState
+import im.tox.hlapi.state.CoreState._
+import im.tox.hlapi.state.FriendState.Friend
+
+import scalaz._
+
+abstract class Parser {
+
+  protected def friendExist(friendNumber: Int, state: ToxState): Boolean = {
+    val friends = CoreState.friendsL.get(state)
+    friends.contains(friendNumber)
+  }
+}
